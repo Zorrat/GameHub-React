30cf6c8904a1c8ed8a67b756ecdd3bd1bb1286e6
Rohan <rohanthorat1230@gmail.com>
2023-07-12 22:41:13 +0530

    added images cards and more

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
+import { Card, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  return <></>;
+  return (
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Heading fontSize={"2xl"}>{game.name}</Heading>
+      <HStack justifyContent="space-between">
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        ></PlatformIconList>
+        <CriticScore score={game.metacritic} />
+      </HStack>
+    </Card>
+  );
 };
 
 export default GameCard;
